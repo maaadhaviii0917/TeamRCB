2207ffa3523dae91ebaf67a9152b1197a8934df7
msunkara <msunkara@demandbase.com>
2023-02-10 18:35:04 +0530

    written pojo for all properties in testdatta file

--- TeamRCBData/src/main/java/com/testvagrant/hackerrank/dto/TeamDTO.java
+++ TeamRCBData/src/main/java/com/testvagrant/hackerrank/dto/TeamDTO.java
@@ -5,6 +5,8 @@ public class TeamDTO {
 	 private String country;
 	 private String role;
 	 private int count;
+	 private String playerName;
+	 private String price;
 
 	    public TeamDTO() {
 	    }
@@ -13,15 +15,15 @@ public class TeamDTO {
 	        return country;
 	    }
 
-	    public void setcountry(String country) {
+	    public void setCountry(String country) {
 	        this.country = country;
 	    }
 	    
-	    public String getrole() {
+	    public String getRole() {
 	        return role;
 	    }
 
-	    public void setrole(String role) {
+	    public void setRole(String role) {
 	        this.role = role;
 	    }
 	    public int getNoofForeigners() {
@@ -30,6 +32,21 @@ public class TeamDTO {
 
 	    public void setNoofForeigners(int count) {
 	        this.count = count;
+	    }
+	    
+	    public String getplayerName() {
+	        return playerName;
+	    }
+
+	    public void setplayerName(String playerName) {
+	        this.playerName = playerName;
+	    }
+	    public String getPrice() {
+	        return price;
+	    }
+
+	    public void setPrice(String price) {
+	        this.price = price;
 	    }
 
 
